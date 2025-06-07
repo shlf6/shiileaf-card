c338fddf45e9f18c16daabddd7493b5548c2f73c
shlf6 <github@shiileaf.dev>
2025-06-07 23:57:53 +0900

    update: 名刺カードに更新

--- public/scripts/fetchImage.js
+++ public/scripts/fetchImage.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  const res = await fetch("https://script.google.com/macros/s/AKfycbyhbfZYUu0GtNLfAxAWpFMqFG7Umg1cDv7H4rFY7uC5-dHu79A_coYDEE0hHMZh74pB/exec");
+  const res = await fetch("https://script.google.com/macros/s/AKfycbwacYIGoetJPZ9OD2U34d2ygiWKhmfOGC5w6ZDEu-bHWLabqrBvApm2lb_u_dmp1U_7/exec");
   const data = await res.json();
   document.getElementById("img-box").src = data.url;
 });
